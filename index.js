6c2cae7733c674a1cb378a53df4267e847729aa0
Eman Gurung <egurung1117@gmail.com>
2023-08-04 22:53:35 -0400

    complete

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// code your solution here
+function superbowlWin(records) {
+    const winRecord = records.find(record => record.result === "W");
+    if (winRecord) {
+      return winRecord.year;
+    } else {
+      return undefined;
+    }
+  }
